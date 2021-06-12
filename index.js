d841ca0c2b975bf2cccef5c1889d1f69d4f9861d
Vivek Raghunathan <vivek.ra@gmail.com>
2021-06-12 13:32:29 +0000

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
-	  const msg = 'Hello Node!\n'
+	  const msg = 'Hello Node! Welcome to App Engine\n'
 	  res.end(msg);
 });
 
