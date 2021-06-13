696a903729a1f8ea42838af40e837fc0217d6f56
karthik-mur <karthik@digitalq.com>
2021-06-13 13:07:29 +0000

    v2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 80;
 
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
-	  const msg = 'Hello Node! Welcome to App Engine v1\n'
+	  const msg = 'Hello Node! Welcome to App Engine v2\n'
 	  res.end(msg);
 });
 
